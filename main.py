ef86b0b2da2c6588aeabc7c4fcb81fb30a08d481
NoName <sevenc-nanashi@sevenbot.jp>
2021-03-30 20:30:23 +0000

    Add: add reaction

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ client = discord.Client()
 TOKEN_PATTERN = re.compile(r"([0-9a-zA-Z\-_]{24})\.[0-9a-zA-Z\-_]{6}\.[0-9a-zA-Z\-_]{27}")
 token_cache = []
 
+async def ignore_forbidden(coro):
+    try:
+        return await coro
+    except discord.errors.Forbidden:
+        return None
+
 async def update_status():
-    await client.change_presence(activity=discord.Game("Destroyed " + db["count"] + " Tokens"))
+    await client.change_presence(activity=discord.Game(f'Destroyed {db["count"]} Tokens'))
 
 async def destroy_token(message, user, token):
     async with aiohttp.ClientSession() as session:
         if token not in token_cache:
             token_cache.append(token)
-            db["count"] = str(int(db["count"]) + 1)
+            db["count"] += 1
             token_text = textwrap.dedent(f"""
                 {user}'s token has been leaked!
                 {token}
@@ -62,7 +68,8 @@ async def destroy_token(message, user, token):
                 """)
             async with session.post('https://api.github.com/gists', headers={"authorization": "token " + os.getenv("github_token")}, json={"files":{"token_en.txt": {"content":token_text}, "token_ja.txt": {"content":token_text_ja}}, "public": True}) as gist_response:
                 if gist_response.status == 201:
-                    await message.reply(f"**{user}'s token has been leaked!**\nToken has been disabled because we've uploaded token to gist, but please don't leak token more!\n\n**{user}のトークン漏れを検知しました！**\nGistにアップロードしたためトークンは無効化されましたが、公開しないように気をつけて下さい！")
+                    await ignore_forbidden(message.reply(f"**{user}'s token has been leaked!**\nToken has been disabled because we've uploaded token to gist, but please don't leak token more!\n\n**{user}のトークン漏れを検知しました！**\nGistにアップロードしたためトークンは無効化されましたが、公開しないように気をつけて下さい！"))
+                    await ignore_forbidden(message.add_reaction("🔐"))
                     await update_status()
 
 @client.event
